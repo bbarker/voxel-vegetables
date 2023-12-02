6e20292eb59fd67ec143f79fb9ee959caaa6a29e
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2023-12-02 23:38:37 +0100

    fix: Put Camera switch into interaction check

--- src/menu.rs
+++ src/menu.rs
@@ -208,14 +208,6 @@ fn click_play_button(
     mut menu_camera_query: Query<&mut Camera, With<MenuCamera>>,
     mut game_camera_query: Query<&mut Camera, (With<VoxelWorldCamera>, Without<MenuCamera>)>,
 ) {
-    // disable menu camera to avoid multiple cameras at once
-    let mut menu_camera = menu_camera_query.single_mut();
-    menu_camera.is_active = false;
-
-    // enable Game Camera
-    let mut game_camera = game_camera_query.single_mut();
-    game_camera.is_active = true;
-
     for (interaction, mut color, button_colors, change_state, open_link) in &mut interaction_query {
         match *interaction {
             Interaction::Pressed => {
