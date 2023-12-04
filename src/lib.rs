ce6fe408869e2111d542f83a653a3be18e307bcd
Brandon Barker <brandon.barker@gmail.com>
2023-12-03 19:07:47 -0500

    fix rebase

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,7 @@
 mod actions;
 mod audio;
 mod camera_handler;
-<<<<<<< HEAD
-mod camera_handler;
 mod lifecycles;
-=======
->>>>>>> db0f4c7 (remove println; fmt)
 mod loading;
 mod menu;
 mod player;
@@ -15,11 +11,7 @@ mod player;
 use crate::actions::ActionsPlugin;
 use crate::audio::InternalAudioPlugin;
 use crate::camera_handler::CameraHandlerPlugin;
-<<<<<<< HEAD
-use crate::camera_handler::CameraHandlerPlugin;
 use crate::lifecycles::LifeCyclesPlugin;
-=======
->>>>>>> db0f4c7 (remove println; fmt)
 use crate::loading::LoadingPlugin;
 use crate::menu::MenuPlugin;
 use crate::player::PlayerPlugin;
